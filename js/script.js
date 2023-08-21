61ead100094c5079146d7137ff3c9ee0627f4c21
sunil khinchi <sunil.khinchi@aurigait.com>
2023-08-21 17:23:27 +0530

    changes of js and html

--- js/script.js
+++ js/script.js
@@ -3,26 +3,41 @@
 
 document.addEventListener("DOMContentLoaded", function(){
     
-    const cards = document.querySelectorAll(".card-box");
+    // const cards = document.querySelectorAll(".card-box");
 
-    const observer = new IntersectionObserver((entries) => {
+    // const observer = new IntersectionObserver((entries) => {
 
-        entries.forEach((entry) => {
-            entry.target.classList.toggle("show", entry.isIntersecting);
-            // if(entry.isIntersecting) observer.unobserve(entry.target);
-        }),
-        {
-            threshold:.5,
+    //     entries.forEach((entry) => {
+    //         entry.target.classList.toggle("show", entry.isIntersecting);
+    //         // if(entry.isIntersecting) observer.unobserve(entry.target);
+    //     }),
+    //     {
+    //         threshold:.5,
 
             
-        }
-        // console.log(entries);
-    });
-    
-   
+    //     }
+    //     // console.log(entries);
+    // });
+    // cards.forEach((card)=>{
+    //        observer.observe(card);
+    // })
 
-    cards.forEach((card)=>{
-           observer.observe(card);
-    })
+let lazyImages = document.querySelectorAll(".image-tag.lazy-load");
+
+let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            let lazyImage = entry.target;
+            console.log(entry);
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.classList.remove("lazy-load");
+            lazyImageObserver.unobserve(lazyImage);
+        }
+    });
+});
+
+lazyImages.forEach(lazyImage => {
+    lazyImageObserver.observe(lazyImage);
+});
   
   });
